8461d8f873c51c8b17f57164a4e10f96f770ee35
saba kevlishvili <sabakevlishvililol@gmail.com>
2018-11-18 20:34:30 +0400

    added a link to the home screen

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,18 +2,33 @@ import React, { Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+
+const Navbarstyle = {
+  color: "white",
+  backgroundColor: "silver",
+  marginTop: 0,
+  paddingTop: 0
+};
+
+const linkStyle = {
+  textDecoration: "none",
+  color: "white"
+};
 
 const Navbar = () => {
   return (
     <div>
-      <AppBar color="default" position="fixed">
+      <AppBar position="fixed" style={Navbarstyle}>
         <Typography
-          variant="headline"
+          variant="h5"
           color="inherit"
           align="center"
           className="fa fa-music"
         >
-          Lyrical
+          <Link style={linkStyle} to="/" variant="headline" align="left">
+            Lyrical
+          </Link>
         </Typography>
       </AppBar>
     </div>
